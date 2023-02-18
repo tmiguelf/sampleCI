8d788e0f5f37f0ed83e4e86ef87946e32207580a
Tiago Freire <tmiguelf@hotmail.com>
2023-02-18 23:06:02 +0100

    backup

--- sample_code/sample_code.py
+++ sample_code/sample_code.py
@@ -1,3 +1,10 @@
 
 def int_to_string(my_int: int) -> str:
-    pass
+    temp = str()
+    div_10 = int(10)
+    while my_int:
+        temp += str(int(my_int % div_10))
+        my_int = int(my_int / div_10)
+
+    out = temp[::-1]
+    return out
