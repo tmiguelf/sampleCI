3655b785301eadaa10fc76294f0cb03c7a1dd7e9
Tiago Freire <tmiguelf@hotmail.com>
2023-03-02 23:22:47 +0100

    initial state

--- sample_code/sample_code.py
+++ sample_code/sample_code.py
@@ -1,10 +1,3 @@
 
 def int_to_string(my_int: int) -> str:
-    temp = str()
-    div_10 = int(10)
-    while my_int:
-        temp += str(int(my_int % div_10))
-        my_int = int(my_int / div_10)
-
-    out = temp[::-1]
-    return out
+    pass
--- sample_code/test_sample.py
+++ sample_code/test_sample.py
@@ -1,16 +1,2 @@
 from sample_code import int_to_string
 
-import unittest
-
-class SampleTest(unittest.TestCase):
-    def test_sample(self):
-        test_cases = {
-            int(1): "1",
-            int(24): "24",
-            }
-
-        for input, expected in test_cases.items():
-            output = int_to_string(input)
-            self.assertEqual(output, expected,
-                f"Case \"{input}\", expected \"{expected}\", got \"{output}\"")
-
